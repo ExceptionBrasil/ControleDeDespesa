273a8373972a51807856566aafc8b0088e655bdf
NerdDPS <daniel.pitthansilveira@gmail.com>
2017-06-22 17:01:01 -0300

    Pre menu

--- ControleDeDespesas/ControleDeDespesas/Controllers/HomeController.cs
+++ ControleDeDespesas/ControleDeDespesas/Controllers/HomeController.cs
@@ -13,5 +13,10 @@ namespace ControleDeDespesas.Controllers
         {
             return View();
         }
+
+        public ActionResult Autentica()
+        {
+            return View();
+        }
     }
 }
