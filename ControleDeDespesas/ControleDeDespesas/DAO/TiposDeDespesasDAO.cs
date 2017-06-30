b70e93cb8b98a42283d179b4d4953ad1bc6183d8
NerdDPS <daniel.pitthansilveira@gmail.com>
2017-06-30 13:49:01 -0300

    alterações pendentes

---
+++ ControleDeDespesas/ControleDeDespesas/DAO/TiposDeDespesasDAO.cs
@@ -0,0 +1,63 @@
+﻿using ControleDeDespesas.Models;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ControleDeDespesas.DAO
+{
+    public class TiposDeDespesasDAO
+    {
+        private ISession session;
+
+        public TiposDeDespesasDAO(ISession sessao)
+        {
+            this.session = sessao;
+        }
+
+
+       public void Inclui (TiposDeDespesas tipo)
+        {
+            ITransaction transacao = session.BeginTransaction();
+            session.Save(tipo);
+            transacao.Commit();
+
+        }
+
+        public void Excluir(TiposDeDespesas tipo)
+        {
+            ITransaction transacao = session.BeginTransaction();
+            session.Delete(tipo);
+            transacao.Commit();
+
+        }
+
+
+        public void Alterar(TiposDeDespesas tipo)
+        {
+            ITransaction transacao = session.BeginTransaction();
+            session.Merge(tipo);
+            transacao.Commit();
+        }
+
+
+        public IList<TiposDeDespesas> Lista()
+        {
+            var lista = session.QueryOver<TiposDeDespesas>()
+                                .List();
+
+            return lista;
+
+        }
+
+        public TiposDeDespesas GetById(int id)
+        {
+            var tipo = session.QueryOver<TiposDeDespesas>()
+                              .Where(t => t.Id == id)
+                              .SingleOrDefault();
+            return tipo;
+        }
+
+    }
+}
---
+++ ControleDeDespesas/ControleDeDespesas/Mappings/TiposDeDespesasMapping.cs
@@ -0,0 +1,19 @@
+﻿using ControleDeDespesas.Models;
+using FluentNHibernate.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ControleDeDespesas.Mappings
+{
+    public class TiposDeDespesasMapping:ClassMap<TiposDeDespesas>
+    {
+        public TiposDeDespesasMapping()
+        {
+            Id(t => t.Id).GeneratedBy.Identity();
+            Map(t => t.Descricao);
+        }
+    
+    }
+}
